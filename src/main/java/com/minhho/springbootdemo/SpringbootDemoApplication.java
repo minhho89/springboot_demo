42eb1261f29cea33167c0d426d52634614dbdb0a
minhho89 <minh.ho89@gmail.com>
2023-08-08 22:48:42 +0700

    Add controller

--- src/main/java/com/minhho/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/minhho/springbootdemo/SpringbootDemoApplication.java
@@ -1,34 +1,15 @@
 package com.minhho.springbootdemo;
 
-import com.minhho.springbootdemo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class SpringbootDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDemoApplication.class, args);
     }
 
-    @GetMapping
-    public List<Student> hello() {
-        return List.of(
-          // new mock data student
-          new Student(
-            1L,
-            "Minh Ho",
-            "minhho@email.com",
-            LocalDate.of(2000, Month.JANUARY, 5),
-                  21
-        ));
-    }
+
 
 }
---
+++ src/main/java/com/minhho/springbootdemo/student/StudentController.java
@@ -0,0 +1,26 @@
+package com.minhho.springbootdemo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/student")
+public class StudentController {
+    @GetMapping
+    public List<Student> getStudents() {
+        return List.of(
+                // new mock data student
+                new Student(
+                        1L,
+                        "Minh Ho",
+                        "minhho@email.com",
+                        LocalDate.of(2000, Month.JANUARY, 5),
+                        21
+                ));
+    }
+}
